47136f75f638f26bbbd93cc8f7f6ed27ef48b16b
Avinav-Dasgupta <127870952+Avinav-Dasgupta@users.noreply.github.com>
2025-04-09 02:26:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// Change theme button
 const changeTheme = () => {
+    // Change theme color based on value of element
     if (document.getElementById("changeTheme").value === "light") {
       document.body.style.backgroundColor = "rgb(34, 33, 33)";
       document.body.style.color = "white";
@@ -9,19 +11,24 @@ const changeTheme = () => {
       document.getElementById("changeTheme").value = "light";
     }
   };
-  
+
+  // Change value of counter based on value of counter
   let count = 0;
+  // Display counter on page
   const updateCounter = () => {
     document.getElementById("counter").innerText = count;
   };
+  // Increase counter of count
   document.getElementById("increaseCounter").addEventListener("click", () => {
     count++;
     updateCounter();
   });
+ // Decrease counter of count
   document.getElementById("decreaseCounter").addEventListener("click", () => {
     count--;
     updateCounter();
   });
+ // Reset counter of count
   document.getElementById("resetCounter").addEventListener("click", () => {
     count = 0;
     updateCounter();
